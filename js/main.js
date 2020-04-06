101982bf633568bb361e7665bb7b1b2bcd6b81c8
Kevin Hill <kahill2014@gmail.com>
2020-04-06 14:07:17 -0500

    Finished home page

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$.backstretch('images/home-background.jpg');
+// Ensure hovering over the text in the home buttons causes the overlay to change
+var homeButtons = document.getElementsByClassName('home-btn');  
+for (i = 0; i < homeButtons.length; i++) {
+	homeButtons[i].addEventListener("mouseover", function(){
+		this.children[1].style.backgroundColor = 'rgba(0,0,0,0)';
+	}, false);
+	homeButtons[i].addEventListener("mouseout", function(){
+		this.children[1].style.backgroundColor = 'rgba(0,0,0,.2)';
+	}, false);
+}
