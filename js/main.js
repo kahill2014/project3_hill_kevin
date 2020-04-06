f33f1fe43fc73c282887e2eb48a5cbe02a6acc55
Kevin Hill <kahill2014@gmail.com>
2020-04-06 16:18:23 -0500

    Updated accordion styles and added cursor change JS

--- js/main.js
+++ js/main.js
@@ -6,13 +6,18 @@ if (document.getElementById("home")){
 // Ensure hovering over the text in the home buttons causes the overlay to change
 var homeButtons = document.getElementsByClassName('home-btn');  
 for (i = 0; i < homeButtons.length; i++) {
-	homeButtons[i].addEventListener("mouseover", function(){
+	homeButtons[i].addEventListener('mouseover', function(){
 		this.children[1].style.backgroundColor = 'rgba(0,0,0,0)';
 	}, false);
-	homeButtons[i].addEventListener("mouseout", function(){
+	homeButtons[i].addEventListener('mouseout', function(){
 		this.children[1].style.backgroundColor = 'rgba(0,0,0,.2)';
 	}, false);
 }
+document.getElementById('fun-otters').addEventListener("click", function(){
+	document.getElementById('facts').style.cursor = 'url(../images/otter-cursor.png), auto';
+}, false);
 $(function(){
-	$( "#accordion" ).accordion();
+	$('#accordion').accordion({ 
+		heightStyle: 'content' 
+	});
 });
